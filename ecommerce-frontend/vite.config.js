7338a61c8690026ba6d93190e819b269e6ae7c13
Singhaditya646 <singhaditya2001aj@gmail.com>
2026-02-27 11:10:42 +0530

    Add SPA redirect for React Router

--- ecommerce-frontend/vite.config.js
+++ ecommerce-frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api' : {
@@ -14,4 +17,6 @@ export default defineConfig({
       }
     }
   }
+  // ✅ This is the key for SPA fallback when building
+  base: '/',
 })
